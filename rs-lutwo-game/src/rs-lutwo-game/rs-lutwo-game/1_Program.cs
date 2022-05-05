4bb1123111c7c84057af85d405b709096f4131ef
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-05-05 22:40:56 +0200

    Add files via upload

---
+++ rs-lutwo-game/src/rs-lutwo-game/rs-lutwo-game/1_Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rs_lutwo_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The_Beginning(); // zawiera 3 instrukcje
+            // kolejna klasa zawiera 7 instrukcji ( ukryte w klasie The_Beggining )
+            // jeszcze kolejna klasa zawiera 5 instrukcji ( dotyczy klasy Main_Quest )
+            //Main_Quest_A.Before_A_Fight();
+            Main_Quest_B.Heal_Improved();
+
+            Console.ReadKey();
+        }
+        static void The_Beginning()
+        {
+            // trzy instrukcje, pierwotnie zaimplementowane w klasie Program
+            // zostały przeniesione do nowej klasy, nazwanej Beginning
+            // [ zrobiono to w celu lepszej przejrzystości kodu ]
+            Beginning.Start_End_Game();
+        }
+    }
+}
---
+++ rs-lutwo-game/src/rs-lutwo-game/rs-lutwo-game/5_Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rs_lutwo_game
+{
+    internal class Enemy : Player
+    {
+        // jest to klasa Enemy z dziedziczeniem z klasy Player
+    }
+}
